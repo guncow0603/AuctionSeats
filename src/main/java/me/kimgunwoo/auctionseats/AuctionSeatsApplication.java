7322b1cd8fdf3de2f74667f99b0b738961b9f0d4
kimgunwoo <guncow0603@gmail.com>
2024-05-29 17:15:38 +0900

    first commit

---
+++ src/main/java/me/kimgunwoo/auctionseats/AuctionSeatsApplication.java
@@ -0,0 +1,13 @@
+package me.kimgunwoo.auctionseats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionSeatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionSeatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/kimgunwoo/auctionseats/AuctionSeatsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kimgunwoo.auctionseats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionSeatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
