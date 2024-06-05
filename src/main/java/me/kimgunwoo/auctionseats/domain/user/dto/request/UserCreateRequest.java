cc49d5023d3e205683fd82148f3c0e6554549401
kimgunwoo <guncow0603@gmail.com>
2024-06-05 16:16:54 +0900

    #3 - [fix-user] request에서 생년월일 입력하는 거 추가함

--- src/main/java/me/kimgunwoo/auctionseats/domain/user/dto/request/UserCreateRequest.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/user/dto/request/UserCreateRequest.java
@@ -1,10 +1,13 @@
 package me.kimgunwoo.auctionseats.domain.user.dto.request;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
 
 public record UserCreateRequest(
         @NotBlank(message = "필수 입력입니다.")
@@ -28,5 +31,11 @@ public record UserCreateRequest(
 
         @NotBlank(message = "필수 입력입니다.")
         @Pattern(regexp = "^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}$", message = "전화번호 형식으로 입력해주세요.")
-        String phoneNumber
+        String phoneNumber,
+
+        @NotBlank(message = "필수 입력입니다.")
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        LocalDate birth
+
+
 ) {}
