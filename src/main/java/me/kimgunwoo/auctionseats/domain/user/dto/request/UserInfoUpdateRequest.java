ed6300c07ff446cbd4b0c6118ef5ae395cf6788e
kimgunwoo <guncow0603@gmail.com>
2024-06-05 15:30:11 +0900

    #2 - [move - user] Request 위치 변경

--- src/main/java/me/kimgunwoo/auctionseats/domain/user/dto/request/UserInfoUpdateRequest.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/user/dto/request/UserInfoUpdateRequest.java
@@ -1,4 +1,4 @@
-package me.kimgunwoo.auctionseats.domain.user.dto;
+package me.kimgunwoo.auctionseats.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
