539f1fd249250be503f45e7cf6d21a06d32a7bf2
kimgunwoo <guncow0603@gmail.com>
2024-06-06 00:59:11 +0900

    #6 [test-user] 유저 서비스 테스트에 displayName 추가

--- src/test/java/me/kimgunwoo/auctionseats/domain/user/service/UserServiceImplTest.java
+++ src/test/java/me/kimgunwoo/auctionseats/domain/user/service/UserServiceImplTest.java
@@ -24,7 +24,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
-
+@DisplayName("User Service Test")
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
 
