996b39689a9deef39097909aaf4c9b5d83098951
kimgunwoo <guncow0603@gmail.com>
2024-06-05 17:17:53 +0900

    #8 - [fix - exception] MethodArgumentNotValidException 경우, 기존 추상 클래스에서 정의된 handler가 있기 때문에 오버라이딩 하여 작성

--- src/main/java/me/kimgunwoo/auctionseats/global/exception/GlobalExceptionHandler.java
+++ src/main/java/me/kimgunwoo/auctionseats/global/exception/GlobalExceptionHandler.java
@@ -2,20 +2,24 @@ package me.kimgunwoo.auctionseats.global.exception;
 
 import me.kimgunwoo.auctionseats.global.dto.EmptyObject;
 import me.kimgunwoo.auctionseats.global.response.ApiResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
 
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     /*
      * ApiException 에 대한 handler
@@ -51,9 +55,10 @@ public class GlobalExceptionHandler {
     /*
      * MethodArgumentNotValidException 에 대한 handler
      * */
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> methodArgumentNotValidHandler(MethodArgumentNotValidException exception) {
-        BindingResult bindingResult = exception.getBindingResult();
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        BindingResult bindingResult = ex.getBindingResult();
         Map<String, String> errors = new HashMap<>();
         bindingResult.getAllErrors().forEach(error ->
                 errors.put(((FieldError)error).getField(), error.getDefaultMessage()));
