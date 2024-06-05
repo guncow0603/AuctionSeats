d85c1b87a693f1bdb5a2729603fe4dad899cb938
kimgunwoo <guncow0603@gmail.com>
2024-06-05 15:35:58 +0900

    #2 - [init - user] 회원 entity에 @comment 애노테이션으로 description 추가

--- src/main/java/me/kimgunwoo/auctionseats/domain/user/entity/User.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/user/entity/User.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import org.hibernate.annotations.Comment;
 
 @Entity
 @Getter
@@ -20,24 +21,31 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Comment("회원 이메일")
     @Column(name = "email")
     private String email;
 
+    @Comment("회원 비밀번호")
     @Column(name = "password")
     private String password;
 
+    @Comment("회원 이름")
     @Column(name = "name")
     private String name;
 
+    @Comment("회원 닉네임")
     @Column(name = "nickname")
     private String nickname;
 
+    @Comment("회원 전화번호")
     @Column(name = "phone_number")
     private String phoneNumber;
 
+    @Comment("회원 역할(관리자 or 일반 유저)")
     @Column(name = "role")
     private Role role = Role.USER;
 
+    @Comment("회원 보유 포인트")
     @Column(name = "point")
     @ColumnDefault("0")
     private long point;
