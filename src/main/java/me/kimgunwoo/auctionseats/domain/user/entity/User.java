1316fc9083d06056c3d7272189568ac26f9425c4
kimgunwoo <guncow0603@gmail.com>
2024-06-05 15:21:36 +0900

    #2 - [init - user] 회원 엔티티 내의 컬럼 추가

--- src/main/java/me/kimgunwoo/auctionseats/domain/user/entity/User.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/user/entity/User.java
@@ -1,4 +1,44 @@
 package me.kimgunwoo.auctionseats.domain.user.entity;
 
+import me.kimgunwoo.auctionseats.domain.user.entity.constant.Role;
+import org.hibernate.annotations.ColumnDefault;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "role")
+    private Role role = Role.USER;
+
+    @Column(name = "point")
+    @ColumnDefault("0")
+    private long point;
 }
