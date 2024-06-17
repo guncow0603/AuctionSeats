17d6f8748ca4cf8bdf18664e2706dbbb924a0c03
kimgunwoo <guncow0603@gmail.com>
2024-06-17 10:25:11 +0900

    #16 [fix-shows] 공연 도메인 구현 - 공연 추가 요청 DTO 수정

--- src/main/java/me/kimgunwoo/auctionseats/domain/show/dto/request/ShowsRequest.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/show/dto/request/ShowsRequest.java
@@ -1,13 +1,30 @@
 package me.kimgunwoo.auctionseats.domain.show.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public record ShowsRequest (
+        @Size(max = 30)
+        @NotBlank
         String name,
+        @Size(max = 150)
+        @NotBlank
         String description,
+
+        @NotBlank
+        @JsonFormat(pattern = "yyyy-MM-dd")
         LocalDate startDate,
+        @NotBlank
+        @JsonFormat(pattern = "yyyy-MM-dd")
         LocalDate endDate,
-        int ageGrade,
-        String runningTime,
+        @NotBlank
+        Integer ageGrade,
+        @NotBlank
+        Integer runningTime,
+        @Size(max = 30)
+        @NotBlank
         String categoryName){
 }
