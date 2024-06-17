2691d42940f3a74276fd192985a6c35c73ca5eb8
kimgunwoo <guncow0603@gmail.com>
2024-06-17 11:52:51 +0900

    #27 [fix-sequence] Request 수정

--- src/main/java/me/kimgunwoo/auctionseats/domain/sequence/dto/request/SequenceRequest.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/sequence/dto/request/SequenceRequest.java
@@ -1,9 +1,15 @@
 package me.kimgunwoo.auctionseats.domain.sequence.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 public record SequenceRequest (
-        int sequence,
+        @NotNull(message = "회차 정보는 필수입니다.")
+        Integer sequence,
+        @NotBlank(message = "공연일시 정보는 필수입니다.")
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
         LocalDateTime startDateTime){}
