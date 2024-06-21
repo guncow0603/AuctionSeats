92b0995c99614f162c6dcebff0239823b009f3ae
kimgunwoo <guncow0603@gmail.com>
2024-06-21 18:55:24 +0900

    #55  [update-reservation] ReservationCreateRequest Validator 추가

--- src/main/java/me/kimgunwoo/auctionseats/domain/reservation/dto/request/ReservationCreateRequest.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/reservation/dto/request/ReservationCreateRequest.java
@@ -1,11 +1,15 @@
 package me.kimgunwoo.auctionseats.domain.reservation.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import me.kimgunwoo.auctionseats.domain.reservation.entity.Reservation;
 import me.kimgunwoo.auctionseats.domain.shows_sequence_seat.entity.ShowsSequenceSeat;
 import me.kimgunwoo.auctionseats.domain.user.entity.User;
 
 public record ReservationCreateRequest (
+        @NotBlank(message = "구역을 입력하세요.")
         String zone,
+        @NotNull(message = "가격을 입력하세요.")
         Long price){
     public Reservation toEntity(User user, ShowsSequenceSeat showsSequenceSeat, Long price) {
         return Reservation.builder()
