f32bc883b2ae2c78b86324f119144f43d7bd7549
kimgunwoo <guncow0603@gmail.com>
2024-06-21 11:48:38 +0900

    #28 [fix-auction] 경매 도메인 수정 - 입찰 엔티티 수정

--- src/main/java/me/kimgunwoo/auctionseats/domain/auction/entity/Bid.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/auction/entity/Bid.java
@@ -1,24 +1,24 @@
 package me.kimgunwoo.auctionseats.domain.auction.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import me.kimgunwoo.auctionseats.domain.user.entity.User;
 import me.kimgunwoo.auctionseats.global.entity.BaseEntity;
 import org.hibernate.annotations.Comment;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Table(name="auction")
+@Table(name = "bid")
 @Entity
 public class Bid extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Comment("입찰 금액")
+    @Column(name = "price", nullable = false, updatable = false)
+    private Long price;
 
     @Comment("입찰한 유저")
     @ManyToOne(fetch = FetchType.LAZY)
@@ -30,12 +30,11 @@ public class Bid extends BaseEntity {
     @JoinColumn(name = "auction_id")
     private Auction auction;
 
-    private Bid(User user, Auction auction) {
+
+    @Builder
+    private Bid(Long price, User user, Auction auction) {
+        this.price = price;
         this.user = user;
         this.auction = auction;
-    }
-
-    public static Bid of(User user, Auction auction) {
-        return new Bid(user, auction);
     }
 }
