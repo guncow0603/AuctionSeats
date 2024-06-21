d77af35f937215c7fa3cb4010d6d822c45fc0f2a
kimgunwoo <guncow0603@gmail.com>
2024-06-21 19:22:40 +0900

    #18 [refactor - user] 토큰 검증 방식 기본으로 돌아가서 재작성, 엑세스 토큰, 리프레시 토큰은 요청 시

--- src/main/java/me/kimgunwoo/auctionseats/global/jwt/JwtAuthorizationFilter.java
+++ src/main/java/me/kimgunwoo/auctionseats/global/jwt/JwtAuthorizationFilter.java
@@ -3,9 +3,6 @@ package me.kimgunwoo.auctionseats.global.jwt;
 
 import java.io.IOException;
 
-import me.kimgunwoo.auctionseats.domain.user.entity.constant.Role;
-import me.kimgunwoo.auctionseats.global.exception.ApiException;
-import me.kimgunwoo.auctionseats.global.security.UserDetailsImpl;
 import me.kimgunwoo.auctionseats.global.util.LettuceUtils;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -16,63 +13,37 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import static me.kimgunwoo.auctionseats.global.exception.ErrorCode.INVALID_TOKEN;
-import static me.kimgunwoo.auctionseats.global.exception.ErrorCode.REQUIRED_LOGIN;
-import static me.kimgunwoo.auctionseats.global.jwt.JwtUtil.ACCESS_TOKEN_HEADER;
-
 @Slf4j(topic = "JWT 토큰 검증 및 인가")
 @RequiredArgsConstructor
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
-
     private final JwtUtil jwtUtil;
     private final LettuceUtils lettuceUtils;
     private final UserDetailsService userDetailsService;
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
         String accessToken = jwtUtil.resolveAccessToken(request);
-        String refreshToken = jwtUtil.resolveRefreshToken(request);
-
-        if (!StringUtils.hasText(accessToken)) {
-            filterChain.doFilter(request, response);
-            return;
-        }
 
         // 엑세스 토큰 검증
-        jwtUtil.validateToken(accessToken);
+        if (StringUtils.hasText(accessToken) && jwtUtil.validateToken(accessToken)) {
+            String username = jwtUtil.getUsernameFromToken(accessToken);
+            String logoutToken = lettuceUtils.get("Logout: " + username);
 
-        // 리프레시 토큰 검증
-        if (!StringUtils.hasText(refreshToken) || jwtUtil.validateToken(refreshToken)) {
-            throw new ApiException(INVALID_TOKEN);
+            // 로그아웃 토큰 검증
+            if (!StringUtils.hasText(logoutToken) || !accessToken.equals(logoutToken)) {
+                setAuthentication(username);
+            }
         }
 
-        // 로그 아웃 되어 있는지 검증
-        if (!lettuceUtils.hasKey(refreshToken)) {
-            throw new ApiException(REQUIRED_LOGIN);
-        }
-
-        // 리프레시 토큰이 만료되지 않았는데, 엑세스 토큰이 없다면 재발급하자
-        response.addHeader(ACCESS_TOKEN_HEADER, reissueAccessToken(refreshToken));
         filterChain.doFilter(request, response);
-    }
-
-    private String reissueAccessToken(String refreshToken) {
-        String username = lettuceUtils.get(refreshToken);
-        UserDetailsImpl userDetails = (UserDetailsImpl)userDetailsService.loadUserByUsername(username);
-        Role role = userDetails.getUser().getRole();
-        String newAccessToken = jwtUtil.createAccessToken(username, role);
-
-        setAuthentication(username);
-        return newAccessToken;
     }
 
     /*
@@ -82,10 +53,8 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         SecurityContext context = SecurityContextHolder.createEmptyContext();
         Authentication authentication = createAuthentication(username);
         context.setAuthentication(authentication);
-
         SecurityContextHolder.setContext(context);
     }
-
     private Authentication createAuthentication(String username) {
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
         return new UsernamePasswordAuthenticationToken(
