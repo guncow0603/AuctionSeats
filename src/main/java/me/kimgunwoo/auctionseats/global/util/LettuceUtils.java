a51fbf320ca055b82a3b584a9fde21f9d34bcc60
kimgunwoo <guncow0603@gmail.com>
2024-06-21 19:20:47 +0900

    #18 [refactor - user] 리프레시 토큰 만료 시간 수정

--- src/main/java/me/kimgunwoo/auctionseats/global/util/LettuceUtils.java
+++ src/main/java/me/kimgunwoo/auctionseats/global/util/LettuceUtils.java
@@ -1,17 +1,13 @@
 package me.kimgunwoo.auctionseats.global.util;
-
 import java.util.concurrent.TimeUnit;
-
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
-
 import lombok.RequiredArgsConstructor;
-
 @Component
 @RequiredArgsConstructor
 public class LettuceUtils {
 
-    public static final int REFRESH_TOKEN_EXPIRATION = 60 * 60 * 24 * 7;
+    public static final int REFRESH_TOKEN_EXPIRATION = 60 * 60 * 24 * 30;
     private final RedisTemplate<String, String> lettuceTemplate;
 
     public void save(String key, String value) {
@@ -22,11 +18,7 @@ public class LettuceUtils {
         return lettuceTemplate.opsForValue().get(key);
     }
 
-    public boolean delete(String key) {
-        return Boolean.TRUE.equals(lettuceTemplate.delete(key));
-    }
-
-    public boolean hasKey(String key) {
-        return Boolean.TRUE.equals(lettuceTemplate.hasKey(key));
+    public void delete(String key) {
+        lettuceTemplate.opsForValue().getOperations().delete(key);
     }
 }
