db76589ec24c08f665ac53d4380c2e6c7bc8fd60
kimgunwoo <guncow0603@gmail.com>
2024-06-21 11:52:36 +0900

    #28 [fix-auction] 경매 도메인 수정 - 경매 엔티티 수정

--- src/main/java/me/kimgunwoo/auctionseats/domain/auction/entity/Auction.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/auction/entity/Auction.java
@@ -2,54 +2,57 @@ package me.kimgunwoo.auctionseats.domain.auction.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import me.kimgunwoo.auctionseats.domain.auction.dto.request.AuctionCreateRequest;
 import me.kimgunwoo.auctionseats.global.entity.BaseEntity;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.Comment;
 
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "auction")
 @Entity
+@Table(name = "auction")
 public class Auction extends BaseEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Comment("시작가")
+    @ColumnDefault("0")
     @Column(name = "start_price", nullable = false)
-    private long startPrice;
+    private Long startPrice = 0L;
 
     @Comment("입찰가")
     @Column(name = "bid_price", nullable = false)
-    private long bidPrice;
+    private Long bidPrice;
     @Comment("시작일시")
     @Column(name = "start_date", nullable = false)
     private LocalDateTime startDateTime;
-
     @Comment("마감일시")
     @Column(name = "end_date", nullable = false)
     private LocalDateTime endDateTime;
 
     @Comment("종료여부 T - 종료 / F - 진행 중")
+    @ColumnDefault("false")
     @Column(name = "is_ended")
-    private boolean isEnded;
+    private Boolean isEnded = false;
 
-    private Auction(long startPrice, LocalDateTime startDateTime, LocalDateTime endDateTime) {
+    @OneToMany(mappedBy = "auction")
+    private Set<Bid> bid = new LinkedHashSet<>();
+
+    @Builder
+    private Auction(Long startPrice, LocalDateTime startDateTime, LocalDateTime endDateTime) {
         this.startPrice = startPrice;
         this.startDateTime = startDateTime;
         this.endDateTime = endDateTime;
     }
 
-    public static Auction of(AuctionCreateRequest request) {
-        return new Auction(
-                request.startPrice(),
-                request.startDateTime(),
-                request.endDateTime()
-        );
+    public void updateBidPrice(Long bidPrice) {
+        this.bidPrice = bidPrice;
     }
 }
