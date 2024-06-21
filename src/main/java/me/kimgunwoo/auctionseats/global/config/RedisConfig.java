1b5363b51780a97f769c857690299c627fc903ee
kimgunwoo <guncow0603@gmail.com>
2024-06-21 19:04:09 +0900

    #18 [refactor - user] config 내에 LettuceConnectionFactory 생성 빈 등록, 리프레시 토큰 시간 수정, redissonClient 에 port와 host 수정

--- src/main/java/me/kimgunwoo/auctionseats/global/config/RedisConfig.java
+++ src/main/java/me/kimgunwoo/auctionseats/global/config/RedisConfig.java
@@ -24,8 +24,13 @@ public class RedisConfig {
     public RedissonClient redissonClient() {
         Config config = new Config();
         config.useSingleServer()
-                .setAddress("redis://localhost:6379");
+                .setAddress("redis://" + host + ":" + port);
         return Redisson.create(config);
+    }
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
@@ -33,8 +38,7 @@ public class RedisConfig {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(new LettuceConnectionFactory(host, port));
-
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory());
         return redisTemplate;
     }
 }
