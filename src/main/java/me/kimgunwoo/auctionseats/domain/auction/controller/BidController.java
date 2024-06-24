111659fa6dcb01511aef971401c9ca962f5bac6a
kimgunwoo <guncow0603@gmail.com>
2024-06-24 15:43:35 +0900

    #55 [update-auction] 경매 도메인 - 입찰하기 기능 구현      - 5% 이상 상회입찰가면 DB저장 후 Redis에  입찰가 갱신

---
+++ src/main/java/me/kimgunwoo/auctionseats/domain/auction/controller/BidController.java
@@ -0,0 +1,33 @@
+package me.kimgunwoo.auctionseats.domain.auction.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import me.kimgunwoo.auctionseats.domain.auction.dto.request.BidRequest;
+import me.kimgunwoo.auctionseats.domain.auction.service.BidService;
+import me.kimgunwoo.auctionseats.domain.user.entity.User;
+import me.kimgunwoo.auctionseats.global.annotaion.CurrentUser;
+import me.kimgunwoo.auctionseats.global.dto.EmptyObject;
+import me.kimgunwoo.auctionseats.global.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static me.kimgunwoo.auctionseats.global.exception.SuccessCode.SUCCESS_BID;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auctions/{auctionId}/bids")
+@RestController
+public class BidController {
+    private final BidService bidService;
+
+    /*입찰하기*/
+    @PostMapping
+    public ResponseEntity<ApiResponse<EmptyObject>> bid(
+            @PathVariable Long auctionId,
+            @Valid @RequestBody BidRequest bidRequest,
+            @CurrentUser User loginUser
+    ) {
+        bidService.bid(auctionId, bidRequest, loginUser);
+        return ResponseEntity.status(SUCCESS_BID.getHttpStatus())
+                .body(ApiResponse.of(SUCCESS_BID.getCode(), SUCCESS_BID.getMessage()));
+    }
+}
