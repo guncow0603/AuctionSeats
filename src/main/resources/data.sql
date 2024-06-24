c48f3223caf135f2519ff397710843f68f324237
kimgunwoo <guncow0603@gmail.com>
2024-06-24 16:39:32 +0900

    #56 [init-admin] data.sql 로 초기 관리자 데이터 넣어주기

---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+insert into users
+(id, created_at, modified_at, birth, email, is_deleted, name, nickname, password, phone_number, role)
+select * from(
+                 select 0 as id,
+                        now() as created_at,
+                        now() as modified_at,
+                        "1999-01-01" as birth,
+                        "admin@gmail.com" as email,
+                        false as is_deleted,
+                        "관리자" as name,
+                        "일번관리자" as nickname,
+                        "$2a$10$70/DaQHbxaRQqYIqPHtLfujXSJAtgV2e2H7DUAzfA8xPgo2YoTK1u" as password,
+                        "01012345678" as phone_number,
+                        "ADMIN" as role
+             ) as init
+where not exists (
+    select 1 from users
+    where users.email = init.email
+);
