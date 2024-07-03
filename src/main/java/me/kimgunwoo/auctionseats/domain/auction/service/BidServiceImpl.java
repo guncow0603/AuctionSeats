b7d9a56f47d685f3a143a80c129b963ae34a9f66
kimgunwoo <guncow0603@gmail.com>
2024-07-03 11:25:01 +0900

    #28  [fix-auction] 입찰하기 redis crud 수정

--- src/main/java/me/kimgunwoo/auctionseats/domain/auction/service/BidServiceImpl.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/auction/service/BidServiceImpl.java
@@ -5,13 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import me.kimgunwoo.auctionseats.domain.auction.dto.request.BidRequest;
 import me.kimgunwoo.auctionseats.domain.auction.entity.Auction;
 import me.kimgunwoo.auctionseats.domain.auction.entity.Bid;
+import me.kimgunwoo.auctionseats.domain.auction.repository.AuctionRedissonRepository;
 import me.kimgunwoo.auctionseats.domain.auction.repository.AuctionRepository;
 import me.kimgunwoo.auctionseats.domain.auction.repository.BidRepository;
 import me.kimgunwoo.auctionseats.domain.user.entity.User;
+import me.kimgunwoo.auctionseats.domain.user.repository.UserRepository;
 import me.kimgunwoo.auctionseats.global.annotaion.DistributedLock;
 import me.kimgunwoo.auctionseats.global.exception.ApiException;
-import org.redisson.api.RAtomicLong;
-import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 
 import static me.kimgunwoo.auctionseats.domain.auction.constant.AuctionConstant.AUCTION_BID_KEY_PREFIX;
@@ -25,30 +25,33 @@ import static me.kimgunwoo.auctionseats.global.exception.ErrorCode.ENDED_AUCTION
 public class BidServiceImpl implements BidService {
 
     private final AuctionRepository auctionRepository;
+    private final UserRepository userRepository;
     private final BidRepository bidRepository;
-    private final RedissonClient redissonClient;
+    private final AuctionRedissonRepository redissonRepository;
 
     @Override
     @DistributedLock(key = "T(me.kimgunwoo.auctionseats.domain.auction.constant.AuctionConstant).AUCTION_BID_KEY_PREFIX.concat(#auctionId)")
     public void bid(Long auctionId, BidRequest bidRequest, User loginUser) {
         String key = AUCTION_BID_KEY_PREFIX + auctionId;
-        RAtomicLong currentBid = redissonClient.getAtomicLong(key);
 
-        if (currentBid.remainTimeToLive() < 1) {
+        if (!redissonRepository.existBucket(key)) {
             throw new ApiException(ENDED_AUCTION);
         }
 
-        Auction auction = auctionRepository.getReferenceById(auctionId);
-        long currentBidPrice = redissonClient.getAtomicLong(key).get();
+        long currentBidPrice = redissonRepository.getValue(key);
         long increaseBidPrice = (long)(currentBidPrice * BID_PRICE_INCREASE_PERCENT);
         long bidPrice = bidRequest.getPrice();
 
-        if (increaseBidPrice > bidPrice) {
-            throw new ApiException(BAD_REQUEST_BID);
-        }
+        //상회입찰이 아니거나, 포인트가 입찰가보다 적은경우
+        long point = userRepository.findPointById(loginUser.getId());
+
+        validateBid(point, increaseBidPrice, bidPrice);
 
         //경매 엔티티 입찰가 갱신 및 입찰 테이블 save
+        Auction auction = auctionRepository.getReferenceById(auctionId);
+
         auction.updateBidPrice(bidPrice);
+
         Bid bid = Bid.builder()
                 .price(bidPrice)
                 .user(loginUser)
@@ -56,6 +59,12 @@ public class BidServiceImpl implements BidService {
                 .build();
 
         bidRepository.save(bid);
-        currentBid.set(bidPrice);
+        redissonRepository.setValue(key, bidPrice);
+    }
+
+    private static void validateBid(long point, long increaseBidPrice, long bidPrice) {
+        if (increaseBidPrice > bidPrice && point >= bidPrice) {
+            throw new ApiException(BAD_REQUEST_BID);
+        }
     }
 }
--- src/main/java/me/kimgunwoo/auctionseats/domain/user/repository/UserRepository.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/user/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package me.kimgunwoo.auctionseats.domain.user.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import me.kimgunwoo.auctionseats.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -14,4 +16,8 @@ public interface UserRepository extends JpaRepository<User,Long> {
 
 
     Optional<User> findByEmail(String email);
+
+    // id를 사용하여 point 조회
+    @Query("SELECT u.point FROM User u WHERE u.id = :userId")
+    Long findPointById(@Param("userId") Long userId);
 }
