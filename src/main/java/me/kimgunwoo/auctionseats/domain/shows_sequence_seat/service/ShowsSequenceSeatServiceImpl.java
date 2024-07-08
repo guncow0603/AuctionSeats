3035b7276afb8afc25ec76067c2ca46ff6240679
kimgunwoo <guncow0603@gmail.com>
2024-07-08 12:35:00 +0900

    #91 [update-admin] ShowsSequenceSeatServiceImpl 쿼리 메서드 기능 업데이트

--- src/main/java/me/kimgunwoo/auctionseats/domain/shows_sequence_seat/service/ShowsSequenceSeatServiceImpl.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/shows_sequence_seat/service/ShowsSequenceSeatServiceImpl.java
@@ -1,9 +1,22 @@
 package me.kimgunwoo.auctionseats.domain.shows_sequence_seat.service;
 
 import lombok.RequiredArgsConstructor;
+import me.kimgunwoo.auctionseats.domain.shows_sequence_seat.entity.ShowsSequenceSeat;
+import me.kimgunwoo.auctionseats.domain.shows_sequence_seat.repository.ShowsSequenceSeatRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ShowsSequenceSeatServiceImpl implements ShowsSequenceSeatService {
+    private final ShowsSequenceSeatRepository showsSequenceSeatRepository;
+
+    public void saveAllShowsSequenceSeat(List<ShowsSequenceSeat> showsSequenceSeatList) {
+        showsSequenceSeatRepository.saveAll(showsSequenceSeatList);
+    }
+
+    public List<ShowsSequenceSeat> findAllBySequenceId(Long sequenceId) {
+        return showsSequenceSeatRepository.findAllBySequenceId(sequenceId);
+    }
 }
