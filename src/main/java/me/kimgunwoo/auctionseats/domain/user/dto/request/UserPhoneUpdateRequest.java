febaf2cc8047fd5e8821af9cf3fac19c110e552c
kimgunwoo <guncow0603@gmail.com>
2024-07-09 09:25:27 +0900

    #6 [update - user] 유저 전화번호 regex 틀릴 시, 메세지 수정

--- src/main/java/me/kimgunwoo/auctionseats/domain/user/dto/request/UserPhoneUpdateRequest.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/user/dto/request/UserPhoneUpdateRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Pattern;
 
 public record UserPhoneUpdateRequest (
         @NotBlank(message = "필수 입력입니다.")
-        @Pattern(regexp = "^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}$", message = "전화번호 형식으로 입력해주세요.")
+        @Pattern(regexp = "^01([0|1|6|7|8|9])?([0-9]{3,4})?([0-9]{4})$", message = "숫자로만 입력해주세요.")
         String phoneNumber,
 
         @NotBlank(message = "필수 입력입니다.")
