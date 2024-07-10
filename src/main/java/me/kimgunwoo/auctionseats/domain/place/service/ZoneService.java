56c3b5655dfc36115becf5c8d63a082b3cbebb5c
kimgunwoo <guncow0603@gmail.com>
2024-07-10 12:36:12 +0900

    #107 [fix-place] ZoneService 수정

--- src/main/java/me/kimgunwoo/auctionseats/domain/place/service/ZoneService.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/place/service/ZoneService.java
@@ -9,5 +9,5 @@ import java.util.List;
 public interface ZoneService {
 
     // 공연장 구역 생성
-    public List<Zone> createZone(Places places, List<ZoneInfo> zoneInfos);
+    List<Zone> createZone(Places places, List<ZoneInfo> zoneInfos);
 }
