f6bc701e923d880cf53242ddab1d8db85d106b5f
kimgunwoo <guncow0603@gmail.com>
2024-07-10 13:04:44 +0900

    #108 [update-shows] 공연 생성 API Controller 업데이트

--- src/main/java/me/kimgunwoo/auctionseats/domain/admin/controller/AdminController.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/admin/controller/AdminController.java
@@ -5,17 +5,19 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.kimgunwoo.auctionseats.domain.admin.adminService.AdminServiceImpl;
 import me.kimgunwoo.auctionseats.domain.admin.dto.request.PlacesRequest;
+import me.kimgunwoo.auctionseats.domain.admin.dto.request.ShowsRequest;
 import me.kimgunwoo.auctionseats.domain.admin.dto.response.PlacesResponse;
+import me.kimgunwoo.auctionseats.global.dto.EmptyObject;
 import me.kimgunwoo.auctionseats.global.response.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 import static me.kimgunwoo.auctionseats.global.exception.SuccessCode.SUCCESS_PLACE_AND_ZONE_CREATE;
+import static me.kimgunwoo.auctionseats.global.exception.SuccessCode.SUCCESS_SHOWS_AND_SCHEDULE_CREATE;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -25,6 +27,7 @@ public class AdminController {
 
     private final AdminServiceImpl adminService;
 
+    // 공연장 및 구역 생성
     @PostMapping("/admin/places")
     public ResponseEntity<ApiResponse<List<PlacesResponse>>> createPlaceAndZone(
             @Valid @RequestBody PlacesRequest placeRequest
@@ -37,6 +40,28 @@ public class AdminController {
                                 SUCCESS_PLACE_AND_ZONE_CREATE.getCode(),
                                 SUCCESS_PLACE_AND_ZONE_CREATE.getMessage(),
                                 placeResponseList)
+                );
+    }
+
+    //  공연과 관련된 공연 정보, 공연 카테고리, 공연 이미지, 공연 및 회차 생성
+    @PostMapping(value = "/admin/places/{placeId}/shows",
+            consumes = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.MULTIPART_FORM_DATA_VALUE
+            })
+    public ResponseEntity<ApiResponse<EmptyObject>> createShowsBundleAndSchedule(
+            @Valid @RequestPart ShowsRequest showsRequest,
+            @RequestPart(value = "files", required = false) List<MultipartFile> multipartFiles,
+            @PathVariable Long placeId
+    ) {
+        adminService.createShowsBundleAndSchedule(placeId, showsRequest, multipartFiles);
+        return ResponseEntity
+                .status(SUCCESS_SHOWS_AND_SCHEDULE_CREATE.getHttpStatus())
+                .body(
+                        ApiResponse.of(
+                                SUCCESS_SHOWS_AND_SCHEDULE_CREATE.getCode(),
+                                SUCCESS_SHOWS_AND_SCHEDULE_CREATE.getMessage()
+                        )
                 );
     }
 
