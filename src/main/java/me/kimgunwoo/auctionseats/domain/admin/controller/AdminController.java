38e5fda6e1691949c21fb4c0f09e8b7dd37bea06
kimgunwoo <guncow0603@gmail.com>
2024-07-12 20:41:05 +0900

    #117 [update-zone_grade] 구역 등급 생성 API 기능 Contoller 업데이트

--- src/main/java/me/kimgunwoo/auctionseats/domain/admin/controller/AdminController.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/admin/controller/AdminController.java
@@ -7,9 +7,11 @@ import me.kimgunwoo.auctionseats.domain.admin.adminService.AdminServiceImpl;
 import me.kimgunwoo.auctionseats.domain.admin.dto.request.GradeRequest;
 import me.kimgunwoo.auctionseats.domain.admin.dto.request.PlacesRequest;
 import me.kimgunwoo.auctionseats.domain.admin.dto.request.ShowsRequest;
+import me.kimgunwoo.auctionseats.domain.admin.dto.request.ZoneGradeRequest;
 import me.kimgunwoo.auctionseats.domain.admin.dto.response.GradeResponse;
 import me.kimgunwoo.auctionseats.domain.admin.dto.response.PlacesResponse;
 import me.kimgunwoo.auctionseats.domain.admin.dto.response.ShowsResponse;
+import me.kimgunwoo.auctionseats.domain.admin.dto.response.ZoneGradeResponse;
 import me.kimgunwoo.auctionseats.global.response.ApiResponse;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -68,7 +70,7 @@ public class AdminController {
                         )
                 );
     }
-
+    // 등급 생성
     @PostMapping("/admin/shows/{showsId}/grades")
     public ResponseEntity<ApiResponse<GradeResponse>> createGrade(
             @PathVariable Long showsId,
@@ -77,12 +79,29 @@ public class AdminController {
         GradeResponse gradeResponse = adminService.createGrade(showsId, gradeRequest);
 
         return ResponseEntity
-                .status(SUCCESS_GRADE.getHttpStatus())
+                .status(SUCCESS_GRADE_CREATE.getHttpStatus())
                 .body(
                         ApiResponse.of(
-                                SUCCESS_GRADE.getCode(),
-                                SUCCESS_GRADE.getMessage(),
+                                SUCCESS_GRADE_CREATE.getCode(),
+                                SUCCESS_GRADE_CREATE.getMessage(),
                                 gradeResponse)
+                );
+    }
+
+    // 구역 등급 생성
+    @PostMapping("/admin/zone-grades")
+    public ResponseEntity<ApiResponse<ZoneGradeResponse>> createZoneGrade(
+            @RequestBody ZoneGradeRequest zoneGradeRequest
+    ) {
+        ZoneGradeResponse zoneGradeResponse = adminService.createZoneGrade(zoneGradeRequest);
+        return ResponseEntity
+                .status(SUCCESS_ZONE_GRADE_CREATE.getHttpStatus())
+                .body(
+                        ApiResponse.of(
+                                SUCCESS_ZONE_GRADE_CREATE.getCode(),
+                                SUCCESS_ZONE_GRADE_CREATE.getMessage(),
+                                zoneGradeResponse
+                        )
                 );
     }
 }
