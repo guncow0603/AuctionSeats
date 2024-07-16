93eaf413853222a53fabee2fb9c6b18d1dbb5da9
kimgunwoo <guncow0603@gmail.com>
2024-07-16 14:28:47 +0900

    #130 [update - shows] ShowsInfoRepository 쿼리 메서드 수정

--- src/main/java/me/kimgunwoo/auctionseats/domain/show/repository/ShowsInfoRepository.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/show/repository/ShowsInfoRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ShowsInfoRepository extends JpaRepository<ShowsInfo, Long> {
-    @Query("select g from ShowsInfo g where g.showsCategory.name = :categoryName")
-    Slice<ShowsInfo> findByCategoryName(Pageable pageable, @Param("categoryName") String categoryName);
+    @Query("select g from ShowsInfo g where (:categoryName is null or g.showsCategory.name = :categoryName)")
+    Slice<ShowsInfo> findAllByCategoryName(Pageable pageable, @Param("categoryName") String categoryName);
 }
