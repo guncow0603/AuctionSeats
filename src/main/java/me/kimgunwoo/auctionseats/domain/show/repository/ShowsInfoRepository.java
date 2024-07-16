cb932705b136c845f1cc127fd8fb954acafc6f4c
kimgunwoo <guncow0603@gmail.com>
2024-07-16 14:23:21 +0900

    #130 [update - shows] 공연 페이징 조회 API ShowsInfoRepository 페이징 쿼리 메소드 구현

--- src/main/java/me/kimgunwoo/auctionseats/domain/show/repository/ShowsInfoRepository.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/show/repository/ShowsInfoRepository.java
@@ -1,7 +1,13 @@
 package me.kimgunwoo.auctionseats.domain.show.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import me.kimgunwoo.auctionseats.domain.show.entity.ShowsInfo;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ShowsInfoRepository extends JpaRepository<ShowsInfo, Long> {
+    @Query("select g from ShowsInfo g where g.showsCategory.name = :categoryName")
+    Slice<ShowsInfo> findByCategoryName(Pageable pageable, @Param("categoryName") String categoryName);
 }
