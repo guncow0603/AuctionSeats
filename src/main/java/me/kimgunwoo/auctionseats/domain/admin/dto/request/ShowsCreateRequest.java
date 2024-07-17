736e885264c99a0d7a5efb880173bc7cb7b4c01b
kimgunwoo <guncow0603@gmail.com>
2024-07-17 17:26:29 +0900

    #142 [fix-admin] 정규식 수정

--- src/main/java/me/kimgunwoo/auctionseats/domain/admin/dto/request/ShowsCreateRequest.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/admin/dto/request/ShowsCreateRequest.java
@@ -15,7 +15,8 @@ import java.time.LocalTime;
 
 public record ShowsCreateRequest(
         @NotNull(message = "공연제목 기입은 필수 입니다.")
-        @Pattern(regexp = "([^\\s-]+(?:\\s+[^\\s-]+)*)\\s*-\\s*([^\\s-]+(?:\\s+[^\\s-]+)*)",
+        @Pattern(
+                regexp = "([A-Za-z0-9\\s(){}<>가-힣]+) - ([A-Za-z0-9\\s(){}<>가-힣]+)",
                 message = "입력 양식: (공연제목) - (지역 및 목표) 입니다.")
         String title,
 
