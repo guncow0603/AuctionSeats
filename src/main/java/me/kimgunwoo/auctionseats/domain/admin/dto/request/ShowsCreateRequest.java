392fe110f3fb6b87737bb35b9ef3b79e6048d736
kimgunwoo <guncow0603@gmail.com>
2024-07-17 17:21:46 +0900

    #142 [refactor-admin] ShowsCreateRequest 리펙토링

--- src/main/java/me/kimgunwoo/auctionseats/domain/admin/dto/request/ShowsCreateRequest.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/admin/dto/request/ShowsCreateRequest.java
@@ -1,7 +1,9 @@
 package me.kimgunwoo.auctionseats.domain.admin.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import me.kimgunwoo.auctionseats.domain.place.entity.Places;
 import me.kimgunwoo.auctionseats.domain.show.entity.Shows;
@@ -12,6 +14,10 @@ import java.time.LocalTime;
 
 
 public record ShowsCreateRequest(
+        @NotBlank(message = "공연제목 기입은 필수 입니다.")
+        @Pattern(regexp = "(\\S+)-(\\S+)", message = "입력 양식: (공연제목)-(지역 및 목표) 입니다.")
+        String title,
+
         @NotNull(message = "공연 시작일 기입은 필수입니다.")
         @JsonFormat(pattern = "yyyy-MM-dd")
         LocalDate startDate,
@@ -24,6 +30,7 @@ public record ShowsCreateRequest(
         public Shows toEntity(Places places, ShowsInfo showsInfo) {
                 return Shows
                         .builder()
+                        .title(title)
                         .places(places)
                         .showsInfo(showsInfo)
                         .startDate(this.startDate)
