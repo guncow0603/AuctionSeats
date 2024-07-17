8dc00519a691ac6aaa6b7980f871f54c18ae0f33
kimgunwoo <guncow0603@gmail.com>
2024-07-17 15:25:43 +0900

    #136 [fix - token] 포인트 정보를 프론트에 넘겨주기 위해 UserDetails 객체에 포인트를 넘겨줬지만, 로그인 이후에 변경되는 것에 대해 실시간 변동 처리가 어려워서 포인트 정보는 따로 가져오도록 수정

--- src/main/java/me/kimgunwoo/auctionseats/domain/user/service/AuthService.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/user/service/AuthService.java
@@ -3,6 +3,7 @@ package me.kimgunwoo.auctionseats.domain.user.service;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import me.kimgunwoo.auctionseats.domain.user.entity.User;
 
 public interface AuthService {
 
@@ -21,4 +22,13 @@ public interface AuthService {
      * @param HttpServletResponse
      * */
     void reissue(HttpServletRequest request, HttpServletResponse response);
+
+    /*
+     * 프론트에서 유저 상태 요청 API 호출 시, 포인트 정보도 함께 보내주기 위한 메서드
+     *
+     * @param User 로그인 유저 정보
+     *
+     * @return Long 현재 유저의 포인트 값
+     * */
+    Long findPoint(User user);
 }
