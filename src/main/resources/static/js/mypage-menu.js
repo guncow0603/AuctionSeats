c9d17ae70566146ea170d611f6e8aaf95568c517
kimgunwoo <guncow0603@gmail.com>
2024-07-19 12:18:03 +0900

    #151 [update] 유저 정보 수정 프론트 구현

--- src/main/resources/static/js/mypage-menu.js
+++ src/main/resources/static/js/mypage-menu.js
@@ -7,7 +7,6 @@ function getMyInfo(token, id) {
         },
         success: function (data) {
             let info = data.data;
-            console.log(info.email)
             $("#info-email").text(info.email);
             $("#info-name").text(info.name);
             $("#info-nickname").text(info.nickname);
@@ -19,4 +18,38 @@ function getMyInfo(token, id) {
             console.error(textStatus);
         },
     })
+}
+
+function updateUserInfo(token, id) {
+    let nickname = $("#update-nickname").val();
+    let phoneNumber = $("#update-phone").val();
+
+    $.ajax({
+        type: "PUT",
+        url: `/api/v1/users/${id}`,
+        contentType: "application/json",
+        headers: {
+            "Authorization": token
+        },
+        data: JSON.stringify({
+            nickname: nickname,
+            phoneNumber: phoneNumber
+        }),
+        success: function (data) {
+            alert("회원 정보를 수정했습니다.")
+            movePageWithToken(`/user-info.html`);
+        },
+        error: function (jqXHR, textStatus) {
+            resetValidationMessages();
+            let response = jqXHR.responseJSON;
+            if (response) {
+                if (response.code.substring(2, 4) === "04") {
+                    $("#update-nickname-span").text(response.message);
+                }
+                if (response.code.substring(2, 4) === "06") {
+                    $("#update-phoneNumber-span").text(response.message);
+                }
+            }
+        }
+    });
 }
