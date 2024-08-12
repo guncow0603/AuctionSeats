6a1a6a85bb62328ab93bfef295a7615b15b43c87
kimgunwoo <guncow0603@gmail.com>
2024-08-12 15:19:53 +0900

    #142 [refactor-payment] dto class 에서 record 로 수정

--- src/main/java/me/kimgunwoo/auctionseats/domain/payment/dto/request/PaymentToApiRequest.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/payment/dto/request/PaymentToApiRequest.java
@@ -1,24 +1,19 @@
 package me.kimgunwoo.auctionseats.domain.payment.dto.request;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import me.kimgunwoo.auctionseats.domain.payment.entity.Payment;
 
 import java.time.LocalDateTime;
 
-@Getter
-@RequiredArgsConstructor
-public class PaymentToApiRequest {
-
-    private final Long amount;
-    private final String orderName;
-    private final String orderId;
-    private final String userEmail;
-    private final String userName;
-    private final String successUrl;
-    private final String failUrl;
-    private final LocalDateTime createdAt;
-
+public record PaymentToApiRequest(
+        Long amount,
+        String orderName,
+        String orderId,
+        String userEmail,
+        String userName,
+        String successUrl,
+        String failUrl,
+        LocalDateTime createdAt
+) {
     public static PaymentToApiRequest from(Payment payment, String successUrl, String failUrl) {
         return new PaymentToApiRequest(
                 payment.getAmount(),
