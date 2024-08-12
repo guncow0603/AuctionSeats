e8b33bfe668641c7bad657243fa853eed50fae45
kimgunwoo <guncow0603@gmail.com>
2024-08-12 15:27:45 +0900

    #142 [refactor-place] dto class 에서 record 로 수정

--- src/main/java/me/kimgunwoo/auctionseats/domain/place/service/PlaceServiceImpl.java
+++ src/main/java/me/kimgunwoo/auctionseats/domain/place/service/PlaceServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-
 @Service
 @RequiredArgsConstructor
 public class PlaceServiceImpl implements PlaceService {
